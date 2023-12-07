640061a4557ab5fef38da176c4a387314c835812
Osigbemhe Daniel <90054585+danielosi2@users.noreply.github.com>
2023-12-07 22:29:46 +0100

    complete

--- lib/currency_page.dart
+++ lib/currency_page.dart
@@ -1,29 +1,65 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 
-class CurrenctConverter extends StatelessWidget {
+class CurrenctConverter extends StatefulWidget {
   const CurrenctConverter({super.key});
 
   @override
+  State<CurrenctConverter> createState() => _CurrenctConverterState();
+}
+
+class _CurrenctConverterState extends State<CurrenctConverter> {
+  double result = 0;
+  final TextEditingController inputText = TextEditingController();
+  void convert() {
+    setState(() {
+      result = double.parse(inputText.text) * 1174;
+    });
+  }
+
+  @override
+  void dispose() {
+    inputText.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
+    final boder = OutlineInputBorder(
+      borderSide: const BorderSide(
+        color: Colors.black,
+        width: 2,
+        style: BorderStyle.solid,
+        strokeAlign: BorderSide.strokeAlignInside,
+      ),
+      borderRadius: BorderRadius.circular(10),
+    );
     return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blueGrey,
+        elevation: 0,
+        title: const Text('Currency converter'),
+        centerTitle: true,
+      ),
       backgroundColor: Colors.blueGrey,
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            const Text(
-              '0',
-              style: TextStyle(
-                fontSize: 45,
-                fontWeight: FontWeight.bold,
-                color: Colors.white,
+        child: Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Text(
+                "NG ${result != 0 ? result.toStringAsFixed(3) : result.toStringAsFixed(0)}",
+                style: const TextStyle(
+                  fontSize: 45,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                ),
               ),
-            ),
-            TextField(
-              style: const TextStyle(color: Colors.black),
-              decoration: InputDecoration(
+              TextField(
+                controller: inputText,
+                style: const TextStyle(color: Colors.black),
+                decoration: InputDecoration(
                   prefixIcon: const Icon(
                     Icons.monetization_on_outlined,
                     color: Colors.black,
@@ -34,20 +70,24 @@ class CurrenctConverter extends StatelessWidget {
                   ),
                   filled: true,
                   fillColor: Colors.white,
-                  focusedBorder: OutlineInputBorder(
-                    borderSide: const BorderSide(
-                      color: Colors.black,
-                      width: 2,
-                      style: BorderStyle.solid,
-                      strokeAlign: BorderSide.strokeAlignInside,
-                    ),
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                  enabledBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(10),
-                  )),
-            )
-          ],
+                  focusedBorder: boder,
+                  enabledBorder: boder,
+                ),
+                keyboardType: const TextInputType.numberWithOptions(decimal: true),
+              ),
+              const SizedBox(height: 10),
+              ElevatedButton(
+                onPressed: convert,
+                style: TextButton.styleFrom(
+                  backgroundColor: Colors.black,
+                  foregroundColor: Colors.white,
+                  minimumSize: const Size(double.infinity, 50),
+                  shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+                ),
+                child: const Text('Convert'),
+              )
+            ],
+          ),
         ),
       ),
     );
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:currency_converter/currency_page.dart';
-import 'package:flutter/cupertino.dart';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -11,6 +11,8 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(home: CurrenctConverter());
+    return const MaterialApp(
+      home: CurrenctConverter(),
+    );
   }
 }
